4ee553bc3f985672acb17166717660ff2af52c29
SymonBezerra <simaobezerratranslate@gmail.com>
2026-03-03 21:55:47 -0300

    fixed compiler to add 8 bytes padding

--- program.py
+++ program.py
@@ -22,38 +22,60 @@ class Program:
         if entry_point not in self.subroutines:
             raise ValueError(f"Entry point '{entry_point}' not found in subroutines")
         subroutines = dict()
+        labels = dict()
         buffer = list()
 
-        def compile(instr):
-            opcode = instr[0]
-            if opcode not in OPCODES:
-                raise ValueError(f"Invalid opcode: {opcode}")
-            if opcode == "GOSUB":
-                target = instr[1]
-                if target not in subroutines:
-                    raise ValueError(
-                        f"GOSUB target '{target}' not found in subroutines or not compiled yet (out of scope)"
+        def compile(sub):
+            for instr in sub:
+                opcode = instr[0]
+                if opcode not in OPCODES:
+                    raise ValueError(f"Invalid opcode: {opcode}")
+                if opcode == "LABEL":
+                    labels[instr[1]] = len(buffer)
+                    continue
+                elif opcode == "JMP":
+                    target = instr[1]
+                    if target not in labels:
+                        raise ValueError(
+                            f"JMP target '{target}' not found in subroutine labels (out of scope)"
+                        )
+                    buffer.append(
+                        struct.pack("<BH", OPCODES["JMP"][0], labels[target]).ljust(
+                            8, b"\x00"
+                        )
                     )
-                elif target == entry_point:
-                    raise ValueError(
-                        "GOSUB cannot call the entry point subroutine (recursive calls not supported)"
+                elif opcode == "GOSUB":
+                    target = instr[1]
+                    if target not in subroutines:
+                        raise ValueError(
+                            f"GOSUB target '{target}' not found in subroutines or not compiled yet (out of scope)"
+                        )
+                    elif target == entry_point:
+                        raise ValueError(
+                            "GOSUB cannot call the entry point subroutine (recursive calls not supported)"
+                        )
+                    buffer.append(
+                        struct.pack(
+                            "<BH", OPCODES["GOSUB"][0], subroutines[target]
+                        ).ljust(8, b"\x00")
                     )
-                return struct.pack("<BH", OPCODES["GOSUB"][0], subroutines[target])
-            else:
-                return struct.pack(OPCODES[opcode][2], OPCODES[opcode][0], *instr[1:])
+                else:
+                    buffer.append(
+                        struct.pack(
+                            OPCODES[opcode][2], OPCODES[opcode][0], *instr[1:]
+                        ).ljust(8, b"\x00")
+                    )
 
         for name, subroutine in self.subroutines.items():
             if name == entry_point:
                 continue
             subroutines[name] = len(buffer)
-            for instr in subroutine:
-                buffer.append(compile(instr))
-        for instr in self.subroutines[entry_point]:
-            subroutines[entry_point] = len(buffer)
-            buffer.append(compile(instr))
+            compile(subroutine)
+        entrypoint_addr = len(buffer)
+        compile(self.subroutines[entry_point])
 
         with open(path, "wb") as f:
             f.write(b"picoASM")
-            f.write(struct.pack("<H", subroutines[entry_point]))
+            f.write(struct.pack("<H", entrypoint_addr))
             for instr in buffer:
                 f.write(instr)
--- subroutine.py
+++ subroutine.py
@@ -38,14 +38,14 @@ class SubRoutine:
                     f"Opcode '{opcode}' expects {OPCODES[opcode][1]} arguments, got {len(args)}"
                 )
 
-            if opcode == "LABEL":
-                self.__labels[args[0]] = len(self.__instructions)
-            elif opcode == "JMP":
-                if args[0] in self.__labels:
-                    self.__instructions.append((opcode, self.__labels[args[0]]))
-                else:
-                    raise ValueError(
-                        f"JMP target '{args[0]}' not found in subroutine labels (out of scope)"
-                    )
-            else:
-                self.__instructions.append(other)
+            # if opcode == "LABEL":
+            #     self.__labels[args[0]] = len(self.__instructions)
+            # elif opcode == "JMP":
+            #     if args[0] in self.__labels:
+            #         self.__instructions.append((opcode, self.__labels[args[0]]))
+            #     else:
+            #         raise ValueError(
+            #             f"JMP target '{args[0]}' not found in subroutine labels (out of scope)"
+            #         )
+            # else:
+            self.__instructions.append(other)
